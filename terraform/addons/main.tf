6a518940789f4b55c13099995b10c2960291b4a9
JustTalhaRehman <talha32rehman@gmail.com>
2026-04-27 18:48:21 +0500

    Add Terraform addons main.tf with Helm and Kubernetes providers

---
+++ terraform/addons/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.12.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "your-terraform-state-bucket"
+    key            = "eks/addons/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "helm" {
+  kubernetes ={
+    host                   = data.terraform_remote_state.cluster.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.cluster.outputs.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.cluster.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.cluster.outputs.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
